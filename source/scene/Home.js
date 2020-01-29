3f0aa796fe74e2cf80639101f751faacd1d6023d
ye0jun <ye0jun@naver.com>
2020-01-29 23:00:12 +0900

    [add] 회원가입 페이지 추가

--- source/scene/Home.js
+++ source/scene/Home.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View, SafeAreaView} from 'react-native';
+import Welcome from '../component/scene/home/Welcome';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Home = () => {
-  return <Text>This is Home</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Welcome name={'진여준'} />
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginLeft: '5%',
+    marginRight: '5%',
+    width: '90%',
+  },
+
+  content: {
+    paddingTop: 20,
+    width: '100%',
+  },
+});
 
 export default Home;
